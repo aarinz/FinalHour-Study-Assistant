d28ebe7ff9fbbf465c0e7f13b010dfc3f1da94a6
Aarin Zacharia <aarinzacharia560@gmail.com>
2024-10-01 12:29:38 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class main {
+    public static void main(String[] args){
+
+        JFrame frame = new JFrame();
+        frame.setTitle("FinalHour Study Assistant");
+        frame.setSize(420,420);
+        frame.setVisible(true);
+    }
+
+}
+
