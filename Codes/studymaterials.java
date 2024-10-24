2668aa84d9477f0ad9340e3a9e34f64616b4a72f
Aarin Zacharia <aarinzacharia560@gmail.com>
2024-10-24 18:26:39 +0530

    upload, clear fix

--- Codes/studymaterials.java
+++ Codes/studymaterials.java
@@ -1,23 +1,35 @@
 package Codes;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.BorderFactory;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.CardLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class studymaterials extends JPanel {
+
+    private JPanel importantSectionPanel, formulaSectionPanel, leftoverSectionPanel;
+    private ArrayList<File> importantFiles, formulaFiles, leftoverFiles;
+    private Map<File, JLabel> fileThumbnails;
 
     public studymaterials(JPanel mainPanel) {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setBackground(Color.black);
         setBorder(BorderFactory.createLineBorder(Color.WHITE, 5));
+
+        importantSectionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        formulaSectionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        leftoverSectionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        importantFiles = new ArrayList<>();
+        formulaFiles = new ArrayList<>();
+        leftoverFiles = new ArrayList<>();
+        fileThumbnails = new HashMap<>();
 
         JLabel titleLabel = new JLabel("Study Materials", JLabel.CENTER);
         titleLabel.setForeground(Color.WHITE);
@@ -32,6 +44,9 @@ public class studymaterials extends JPanel {
         JButton clearFormulaButton = new JButton("Clear Files");
         JButton clearLeftoverButton = new JButton("Clear Files");
 
+        JButton saveButton = new JButton("Save");
+        JButton loadButton = new JButton("Load");
+
         JButton backButton = new JButton("Back");
         backButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
 
@@ -41,25 +56,37 @@ public class studymaterials extends JPanel {
         styleButton(clearImportantButton);
         styleButton(clearFormulaButton);
         styleButton(clearLeftoverButton);
+        styleButton(saveButton);
+        styleButton(loadButton);
         styleButton(backButton);
 
         add(Box.createRigidArea(new Dimension(0, 20)));
         add(titleLabel);
         add(Box.createRigidArea(new Dimension(0, 50)));
-        addSection("Important Portion", uploadImportantButton, clearImportantButton);
+        addSection("Important Portion", uploadImportantButton, clearImportantButton, importantSectionPanel);
         add(Box.createRigidArea(new Dimension(0, 30)));
-        addSection("Formulas", uploadFormulaButton, clearFormulaButton);
+        addSection("Formulas", uploadFormulaButton, clearFormulaButton, formulaSectionPanel);
         add(Box.createRigidArea(new Dimension(0, 30)));
-        addSection("Leftover Portion", uploadLeftoverButton, clearLeftoverButton);
+        addSection("Leftover Portion", uploadLeftoverButton, clearLeftoverButton, leftoverSectionPanel);
         add(Box.createRigidArea(new Dimension(0, 50)));
+
+        JPanel saveLoadPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        saveLoadPanel.add(saveButton);
+        saveLoadPanel.add(loadButton);
+        saveLoadPanel.setBackground(Color.black);
+        add(saveLoadPanel);
+
         add(backButton);
         
-        backButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ((CardLayout) mainPanel.getLayout()).show(mainPanel, "main");
-            }
-        });
+        backButton.addActionListener(e -> ((CardLayout) mainPanel.getLayout()).show(mainPanel, "main"));
+
+        uploadImportantButton.addActionListener(e -> uploadFile(importantFiles, importantSectionPanel));
+        uploadFormulaButton.addActionListener(e -> uploadFile(formulaFiles, formulaSectionPanel));
+        uploadLeftoverButton.addActionListener(e -> uploadFile(leftoverFiles, leftoverSectionPanel));
+
+        clearImportantButton.addActionListener(e -> clearFiles(importantFiles, importantSectionPanel));
+        clearFormulaButton.addActionListener(e -> clearFiles(formulaFiles, formulaSectionPanel));
+        clearLeftoverButton.addActionListener(e -> clearFiles(leftoverFiles, leftoverSectionPanel));
     }
 
     private void styleButton(JButton button) {
@@ -69,17 +96,87 @@ public class studymaterials extends JPanel {
         button.setAlignmentX(JButton.CENTER_ALIGNMENT);
     }
 
-    private void addSection(String sectionTitle, JButton uploadButton, JButton clearButton) {
+    private void addSection(String sectionTitle, JButton uploadButton, JButton clearButton, JPanel sectionPanel) {
         JLabel sectionLabel = new JLabel(sectionTitle, JLabel.LEFT);
         sectionLabel.setForeground(Color.WHITE);
         sectionLabel.setFont(new Font("Arial", Font.PLAIN, 25));
         sectionLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-        uploadButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-        clearButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        
+        sectionPanel.setPreferredSize(new Dimension(800, 100));
+        sectionPanel.setBackground(Color.black);
 
         add(sectionLabel);
         add(Box.createRigidArea(new Dimension(0, 10)));
+        add(sectionPanel);
         add(uploadButton);
         add(clearButton);
+    }
+
+    private void uploadFile(ArrayList<File> fileList, JPanel sectionPanel) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setMultiSelectionEnabled(true);
+        int option = fileChooser.showOpenDialog(this);
+        if (option == JFileChooser.APPROVE_OPTION) {
+            File[] files = fileChooser.getSelectedFiles();
+            for (File file : files) {
+                fileList.add(file);
+                displayFile(file, sectionPanel);
+            }
+        }
+    }
+
+    private void displayFile(File file, JPanel sectionPanel) {
+        String fileName = file.getName().toLowerCase();
+        
+        if (fileName.endsWith(".png") || fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) {
+            ImageIcon thumbnailIcon = new ImageIcon(new ImageIcon(file.getPath()).getImage()
+                    .getScaledInstance(100, 100, Image.SCALE_SMOOTH));
+            JLabel imageLabel = new JLabel(thumbnailIcon);
+            sectionPanel.add(imageLabel);
+            sectionPanel.revalidate();
+            sectionPanel.repaint();
+            
+            fileThumbnails.put(file, imageLabel);
+
+            imageLabel.addMouseListener(new MouseAdapter() {
+                public void mouseClicked(MouseEvent evt) {
+                    showResizableImage(file);
+                }
+            });
+        } else if (fileName.endsWith(".pdf") || fileName.endsWith(".ppt") || fileName.endsWith(".pptx")) {
+            JLabel fileLabel = new JLabel(file.getName());
+            fileLabel.setForeground(Color.WHITE);
+            sectionPanel.add(fileLabel);
+            sectionPanel.revalidate();
+            sectionPanel.repaint();
+
+            fileThumbnails.put(file, fileLabel);
+
+            fileLabel.addMouseListener(new MouseAdapter() {
+                public void mouseClicked(MouseEvent evt) {
+                    try {
+                        Desktop.getDesktop().open(file);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    private void showResizableImage(File file) {
+        ImageIcon fullSizeIcon = new ImageIcon(file.getPath());
+        JLabel fullImageLabel = new JLabel(fullSizeIcon);
+        JScrollPane scrollPane = new JScrollPane(fullImageLabel);
+        scrollPane.setPreferredSize(new Dimension(1300, 1000));
+        scrollPane.setViewportView(fullImageLabel);
+        JOptionPane.showMessageDialog(null, scrollPane, "Full View", JOptionPane.PLAIN_MESSAGE);
+    }
+
+    private void clearFiles(ArrayList<File> fileList, JPanel sectionPanel) {
+        fileList.clear();
+        sectionPanel.removeAll();
+        sectionPanel.revalidate();
+        sectionPanel.repaint();
     }
 }
